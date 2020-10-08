f93dae52f3ed13c01820f750480dedc42d36cd4f
jlplenio <ubtown@gmail.com>
2020-10-08 22:00:12 +0200

    restructure, url regex check

--- app.py
+++ app.py
@@ -1,31 +1,20 @@
 from flask import Flask, render_template, request
 
-from grabber import Grabber
+from util import build_post_response
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
-    # Todo: cut ur and check with ^[a-zA-Z0-9]{7}$
 
+@app.route('/', methods=['POST', 'GET'])
+def index():
     if request.method == 'POST':
-        link_list = ""
-        message = ""
-        imgur_url = request.form['imgur_url_field']
-        try:
-            link_list = Grabber.get_direct_links(imgur_url)
-            link_list = [link+"\n" for link in link_list]
-        except Exception as e:
-            message = "There was an error " + str(e)
-        return render_template('index.html',
-                            link_list=link_list,
-                            message=message,
-                            imgur_url=imgur_url)
-    else: 
+        return build_post_response(request)
+    else:
         return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
---
+++ util.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+from flask import render_template
+
+
+class Grabber:
+    header = {
+        'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, "
+                      "like Gecko) Chrome/57.0.2987.98 Safari/537.36"}
+    img_url_template = "https://i.imgur.com/{}.jpg"
+
+    @classmethod
+    def get_direct_links(cls, url):
+        session = requests.Session()
+        response = session.post(url, headers=cls.header)  # Todo, check page status
+
+        soup = BeautifulSoup(response.text, "html.parser")
+        found_images = soup.findAll("div", itemtype="http://schema.org/ImageObject")
+        images_codes = [found_image.get("id") for found_image in found_images]
+        direct_links = [cls.img_url_template.format(code) for code in images_codes]
+        return direct_links
+
+
+def verify_link(url):
+    re_url_pattern = "^https://imgur.com/(a|gallery)/[a-zA-Z0-9]{7}$"
+    import re
+    if re.match(re_url_pattern, url):
+        return True
+    return False
+
+
+def build_post_response(request):
+    link_list = ""
+    message = ""
+    imgur_url = request.form['imgur_url_field']
+
+    if not verify_link(imgur_url):
+        return render_template('index.html', message="no valid imgur url found")
+
+    try:
+        link_list = Grabber.get_direct_links(imgur_url)
+        link_list = [link + "\n" for link in link_list]
+    except Exception as e:
+        message = "an unexpected error occurred " + str(e)
+    return render_template('index.html',
+                           link_list=link_list,
+                           message=message,
+                           imgur_url=imgur_url)
