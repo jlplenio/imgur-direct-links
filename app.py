bb17a226a4e45e9a3c577cc0e4eb21d419df3196
jlplenio <ubtown@gmail.com>
2020-10-09 00:16:31 +0200

    volatile usage counter

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import sys
+from multiprocessing import Value
+
 from flask import Flask, render_template, request
 
 from util import build_post_response
@@ -6,6 +9,9 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
+
+# volatile visit counter
+counter = Value('i', 0)
 
 # rate limiting
 limiter = Limiter(
@@ -17,6 +23,11 @@ limiter = Limiter(
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    with counter.get_lock():
+        counter.value += 1
+        print("Visit", counter.value)
+        sys.stdout.flush()
+
     if request.method == 'POST':
         return build_post_response(request)
     else:
