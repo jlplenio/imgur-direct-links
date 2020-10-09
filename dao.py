7b2f8d55ca3efa8a7733532b1614ee49e9e4b77e
jlplenio <ubtown@gmail.com>
2020-10-10 00:56:39 +0200

    potential postgresql lockout fix

--- dao.py
+++ dao.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 class Database:
@@ -9,6 +10,7 @@ class Database:
     def __init__(self):
         self.database_url = os.environ['DATABASE_URL']
         self.connection = psycopg2.connect(self.database_url, sslmode='require')
+        self.connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         self.cursor = self.connection.cursor()
 
     def query(self, query):
@@ -19,7 +21,6 @@ class Database:
             return None
 
     def __del__(self):
-        self.connection.commit()
         self.connection.close()
 
 
@@ -29,9 +30,9 @@ class Dao:
         self.database = Database()
 
     def get_counter(self, name):
-        logging.debug("List of users requested")
+        logging.debug(f"counter {name} requested")
         return self.database.query("SELECT {} from counters WHERE id=1".format(name))
 
     def inc_counter(self, name):
-        logging.debug("List of users requested")
+        logging.debug(f"counter {name} incremented")
         return self.database.query("UPDATE counters SET {0} = {0} + 1 WHERE id=1".format(name))
