c4d6251a8770481f358d3cb16dce25941faa0d85
jlplenio <ubtown@gmail.com>
2020-10-10 17:50:20 +0200

    added shuffling and tagging functionality

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
+import os
+
 from flask import Flask, render_template, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 from dao import Dao
-from util import build_post_response
+from util import get_links, handle_extras
 
 app = Flask(__name__)
-version = "v0.3"
+version = "v0.4"
 
 # rate limiting
 limiter = Limiter(
     app,
     key_func=get_remote_address,
-    default_limits=["10 per minute"],
+    default_limits=["15 per minute"],
     )
 
 # data access object
@@ -24,10 +26,12 @@ def index():
     render_parameters = {}
 
     if request.method == 'POST':
-
-        render_parameters = build_post_response(request)
-        if 'link_list' in render_parameters:
-            dao.inc_counter("actions")
+        if 'button_get_links' in request.form:
+            render_parameters = get_links(request)
+            # if 'link_list' in render_parameters:
+            #     dao.inc_counter("actions")
+        elif 'button_extra' in request.form.keys():
+            render_parameters = handle_extras(request)
 
     return render_template('index.html',
                            actions=dao.get_counter("actions")[0][0],
@@ -36,4 +40,6 @@ def index():
 
 
 if __name__ == '__main__':
+    if os.environ.get('flask_debug'):
+        app.debug = True
     app.run()
